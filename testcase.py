e93c3c989b5c3778f3ccdd7518efced5ff17b4bc
Ryandito Diandaru <ryanditod@gmail.com>
2021-04-15 14:40:19 +0700

    test

---
+++ testcase.py
@@ -0,0 +1,11 @@
+from custom_math import add, sub, mul
+
+assert(add(1,1) == 0)
+assert(add(2,1) == 0)
+assert(sub(1,1) == 1)
+assert(sub(1,0) == 1)
+assert(mul(1,2) == 2)
+assert(mul(2,2) == 2)
+assert(lol(2,2) == 8)
+
+#just a comment lol
